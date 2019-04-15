11eda1f3d78b7698b25a4bc58e267bb6b92003fa
redhawk16 <thomasgameone@gmail.com>
2019-04-15 20:50:40 +0600

    Project complete

--- contacts.php
+++ contacts.php
@@ -9,7 +9,7 @@
 			<h2 class="title">Контакты</h2>
 
 			<div class="contacts-left">
-				<p style="text-indent: 1rem; margin-bottom: 2rem">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, at! Accusantium obcaecati libero sequi molestiae. Quis adipisci accusamus a, officiis repellendus ad sapiente atque obcaecati. Exercitationem accusamus, dolorum nemo mollitia culpa laborum, officia consequuntur commodi saepe voluptatibus alias, nobis a.</p>
+				<p style="text-indent: 1rem; margin-bottom: 2rem">Мы работаем с понедельника по пятницу, с 10 до 19 часов. Без перерывов. Проезд на шаттле: заезд со стороны Солнечной системы на основную парковку Земля через пояс астеройдов (парковка платная — 100р. за 2 часа), проезд прямо мимо Солнца и Луны на Паркинг №2. неОфициальный магазин «Процессоры со всего Мира».</p>
 				<p style="font-weight: 600; margin-bottom: 2rem">Нужна помощь? Свяжитесь с нами!<br>
 				<a class="email" href="mailto:pochekuev@me.com" style="font-weight: 400"><i class="fas fa-envelope"></i>pochekuev@me.com</a></p>
 				<p style="font-weight: 600; margin-bottom: 2rem">Наш телефон для связи<br>
